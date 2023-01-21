d5af2cfdf61e52927d16fb2e5d30620368dd3758
daehan <dh16931@gmail.com>
2023-01-21 21:56:08 +0900

    #1 feat: Upload file component

--- src/Components/Dailly/DailyForm.tsx
+++ src/Components/Dailly/DailyForm.tsx
@@ -1,20 +1,29 @@
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
 import { MealType } from "../../@types/enums";
+import { Button, FileButton } from "@mantine/core";
 
 const DailyForm: FC = () => {
+  const [file, setFile] = useState<File | null>(null);
   return (
     <div>
       <h4>{MealType.Fasting}</h4>
       <input type="number" defaultValue={80} min={0} max={1000}></input>
+      <FileButton onChange={setFile} accept="image/png,image/jpeg">
+        {(props) => <Button {...props}>Upload image</Button>}
+      </FileButton>
+      <Button>Save</Button>
 
       <h4>{MealType.Breakfast}</h4>
       <input type="number" defaultValue={100} min={0} max={1000}></input>
+      <Button>Save</Button>
 
       <h4>{MealType.Lunch}</h4>
       <input type="number" defaultValue={100} min={0} max={1000}></input>
+      <Button>Save</Button>
 
       <h4>{MealType.Snack}</h4>
       <input type="number" defaultValue={100} min={0} max={1000}></input>
+      <Button>Save</Button>
     </div>
   );
 };
