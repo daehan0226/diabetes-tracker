defa91685eaa8d0261bf8083b03f744b0d9bd498
daehan <dh16931@gmail.com>
2023-01-22 11:28:23 +0900

    #3 feat: footer, loging component

---
+++ src/Components/Auth/Login.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Container, ScrollArea, Badge, Text, Flex } from "@mantine/core";
+
+function Login() {
+  return <Container> login </Container>;
+}
+
+export default Login;
