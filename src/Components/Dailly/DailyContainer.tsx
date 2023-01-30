caa0e67f1d88894e84f145d497f7ccc16fde9bf1
daehan <dh16931@gmail.com>
2023-01-30 13:36:36 +0900

    feat: edit component
    #9 feat: update on table cell

--- src/Components/Dailly/DailyContainer.tsx
+++ src/Components/Dailly/DailyContainer.tsx
@@ -1,38 +1,12 @@
-import React, { FC, useEffect, useState } from "react";
-import { IDailyTrackInfo, MealType } from "../../@types";
+import React, { FC, useState } from "react";
 import DailyBox from "./DailyBox";
 import DailyTable from "./DailyTable";
 import { Button, Container } from "@mantine/core";
 import { useNavigate } from "react-router-dom";
-import { getTracking } from "../../Apis";
-import { useAuthState } from "../../Hookes";
-import { OrderByArray } from "../../Helper/sortHelper";
 
 const DailyContainer: FC = () => {
   const navigate = useNavigate();
-  const state = useAuthState();
   const [displayTable, setDisplayTable] = useState<boolean>(false);
-  const [data, setData] = useState<IDailyTrackInfo[]>([]);
-
-  useEffect(() => {
-    async function fetchTracking() {
-      const data = await getTracking(state.userId);
-      const dates = Array.from(new Set(data.map((item: any) => item.date)));
-      let format: IDailyTrackInfo[] = [];
-      for (const date of dates) {
-        format.push({
-          date,
-          trackingInfo: OrderByArray(
-            data.filter((d) => d.date === date),
-            "type" as MealType
-          ),
-        });
-      }
-      console.log(format);
-      setData([...format]);
-    }
-    fetchTracking();
-  }, []);
 
   return (
     <Container mt={0} p={0}>
@@ -42,7 +16,7 @@ const DailyContainer: FC = () => {
       <Button m={16} onClick={() => setDisplayTable(!displayTable)}>
         {displayTable ? "With images" : "Only numbers"}
       </Button>
-      {displayTable ? <DailyTable data={data} /> : <DailyBox></DailyBox>}
+      {displayTable ? <DailyTable /> : <DailyBox />}
     </Container>
   );
 };
