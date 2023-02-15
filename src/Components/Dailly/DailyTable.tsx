a640b9bbca7234e378cd0edc61a116c295ec93b4
daehan <dh16931@gmail.com>
2023-02-15 14:04:08 +0900

    feat: pick month for table

--- src/Components/Dailly/DailyTable.tsx
+++ src/Components/Dailly/DailyTable.tsx
@@ -1,7 +1,7 @@
 import React, { FC, useState } from "react";
-import { Table } from "@mantine/core";
+import { Select, Table } from "@mantine/core";
 import { IDailyTrackInfo, ITrackingInfo, MealType } from "../../@types";
-import { useAuthState, useRecordState } from "../../Hookes";
+import { useRecordState } from "../../Hookes";
 import { OrderByArray, setDateFormat } from "../../Helper";
 import { DatePicker } from "@mantine/dates";
 import { TableRow } from "./Table";
@@ -34,11 +34,51 @@ const setDailyData = (
   return format;
 };
 
+const minYear = 2023;
+const currentYear = new Date().getFullYear();
 const minDate = "2023-01-01";
+const maxYearMonth = new Date().toISOString().slice(0, 7);
 const cols = ["Date", "Fasting", "Breakfast", "Lunch", "Dinner"];
+
+const getMonths = () => {
+  let result = [];
+  let years = [];
+  let months = [
+    "01",
+    "02",
+    "03",
+    "04",
+    "05",
+    "06",
+    "07",
+    "08",
+    "09",
+    "10",
+    "11",
+    "12",
+  ];
+  let count = 0;
+  while (minYear + count <= currentYear) {
+    years.push(minYear);
+    count++;
+  }
+  for (const year of years) {
+    for (const month of months) {
+      const value = `${String(year)}-${month}`;
+      if (value <= maxYearMonth) {
+        result.push({
+          value,
+          label: value,
+        });
+      }
+    }
+  }
+  return result;
+};
 
 const DailyTable: FC = () => {
   const [data, setData] = useState<IDailyTrackInfo[]>([]);
+  const [month, setMonth] = useState<string | null>(null);
   const [addDate, setAddDate] = useState<Date>(new Date());
   const recordState = useRecordState();
 
@@ -75,6 +115,7 @@ const DailyTable: FC = () => {
         minDate={new Date(minDate)}
         maxDate={new Date()}
       />
+      <Select placeholder="Pick month" data={getMonths()} onChange={setMonth} />
       <Table withBorder withColumnBorders>
         <thead>
           <tr>
@@ -84,7 +125,10 @@ const DailyTable: FC = () => {
           </tr>
         </thead>
         <tbody>
-          <TableRow data={data} refresh={fetchTracking} />
+          <TableRow
+            data={month ? data.filter((r) => r.date.startsWith(month)) : data}
+            refresh={fetchTracking}
+          />
         </tbody>
       </Table>
     </>
